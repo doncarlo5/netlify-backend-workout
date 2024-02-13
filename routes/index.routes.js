2d685247ff7d571b056c8df656932bc1660a98a4
doncarlos5 <pro.julien.thomas@gmail.com>
2024-02-13 18:53:09 +0100

    Refactor routes in index.routes.js

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,10 +1,18 @@
 const router = require("express").Router();
-const authRouter = require("./auth.routes");
+const isAuthenticated = require("../middleware/isAuthenticated");
 
 router.get("/", (req, res, next) => {
   res.json("All good in here");
 });
 
-router.use("/auth", authRouter);
+router.use("/auth", require("./auth.routes"));
+
+router.use(
+  "/exercise-user",
+  isAuthenticated,
+  require("./exercise-user.routes")
+);
+
+router.use("/exercise-type", require("./exercise-type.routes")); //! Should be in exercise user
 
 module.exports = router;
