918e2817c99d3f64691cf0bb2bb8d591c951d4da
doncarlos5 <pro.julien.thomas@gmail.com>
2024-02-15 16:28:51 +0100

    Remove commented code and unused imports in exercise-type.model.js

--- models/exercise-type.model.js
+++ models/exercise-type.model.js
@@ -11,11 +11,6 @@ const exerciseType = new Schema({
     enum: ["bar", "free-weight", "cable", "machine"],
     required: true,
   },
-  // image: {
-  //   type: String,
-  //   default:
-  //     "https://toppng.com/uploads/preview/weights-svg-vector-weightlifting-icon-11553466117vxf3vpx9io.png",
-  // },
   advice: {
     type: String,
     required: true,
@@ -42,25 +37,3 @@ const exerciseType = new Schema({
 const ExerciseType = model("ExerciseType", exerciseType);
 
 module.exports = ExerciseType;
-
-// nom
-// advice
-// timer
-
-// find the exercise by id .populate exercise type
-
-// 1. recuperer une seesion par son id
-// 2. populate les exercices de la session
-// 3. dans exercice id, populate le type d'exercice
-
-// SessionModel
-//   .findById()
-//   .populate({
-//     path : 'exercice_ids',
-//     populate : {
-//       path : 'exercicetype'
-//     }
-//   })
-//   .exec(function (err, res) {
-
-//   })
