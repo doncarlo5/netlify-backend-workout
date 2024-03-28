bcd93d520ccfb5d1a4bde459cbb6c4a77b57bafd
doncarlos5 <pro.julien.thomas@gmail.com>
2024-03-28 18:05:19 +0100

    Remove date from exercise-user model

--- models/exercise-user.model.js
+++ models/exercise-user.model.js
@@ -2,10 +2,6 @@ const { Schema, model } = require("mongoose");
 
 const exerciseUserSchema = new Schema(
   {
-    date: {
-      type: Date,
-      required: [true, "Date is required."],
-    },
     type: {
       type: Schema.Types.ObjectId,
       ref: "ExerciseType",
