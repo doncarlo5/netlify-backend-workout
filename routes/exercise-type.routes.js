81387a675daaebc802b00c9b39e91f27e09d8aff
doncarlos5 <pro.julien.thomas@gmail.com>
2024-04-13 18:32:16 +0200

    Update exercise-type.routes.js to include type_session field in create, update, and get endpoints

--- routes/exercise-type.routes.js
+++ routes/exercise-type.routes.js
@@ -24,37 +24,34 @@ router.get("/", async (req, res, next) => {
 
 router.post("/", async (req, res, next) => {
   try {
-    const { name, type, advice, timer, repRange1, repRange2, repRange3 } =
-      req.body;
+    const {
+      name,
+      advice,
+      timer,
+      repRange1,
+      repRange2,
+      repRange3,
+      type_session,
+    } = req.body;
 
     if (
-      !name ||
-      !type ||
-      !advice ||
-      !timer ||
-      !repRange1 ||
-      !repRange2 ||
-      !repRange3
+      (!name || !timer || !repRange1 || !repRange2 || !repRange3, !type_session)
     ) {
       return res.status(400).json({ message: "Missing fields" });
     }
 
-    if (name || type || advice !== typeof String) {
-      return res.status(400).json({ message: "Name should be a string" });
-    }
-
-    if (timer || repRange1 || repRange2 || repRange3 !== typeof Number) {
-      return res.status(400).json({ message: "Timer should be a number" });
+    if (timer !== typeof Number) {
+      return res.status(400).json({ message: "Should be a number" });
     }
 
     const createExerciseType = await ExerciseType.create({
       name: name,
-      type: type,
       advice: advice,
       timer: timer,
       repRange1: repRange1,
       repRange2: repRange2,
       repRange3: repRange3,
+      type_session: type_session,
       owner: req.user._id,
     });
 
@@ -64,8 +61,90 @@ router.post("/", async (req, res, next) => {
   }
 });
 
-// add delete
+// Get one exercise type
 
-// add update
+router.get("/:id", async (req, res, next) => {
+  try {
+    const oneExerciseType = await ExerciseType.findOne({
+      owner: req.user._id,
+      _id: req.params.id,
+    });
+
+    if (!oneExerciseType) {
+      return res
+        .status(400)
+        .json({ message: "Exercise Type - Unauthorized or not found" });
+    }
+    res.json(oneExerciseType);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Update an exercise type
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    const {
+      name,
+      advice,
+      timer,
+      repRange1,
+      repRange2,
+      repRange3,
+      type_session,
+    } = req.body;
+
+    if (
+      (!name || !timer || !repRange1 || !repRange2 || !repRange3, !type_session)
+    ) {
+      return res
+        .status(400)
+        .json({ message: "Trying to update - Missing fields" });
+    }
+
+    if (timer !== typeof Number) {
+      return res.status(400).json({ message: "Should be a number" });
+    }
+
+    const updateExerciseType = await ExerciseType.findByIdAndUpdate(
+      { _id: req.params.id },
+      {
+        name: name,
+        advice: advice,
+        timer: timer,
+        repRange1: repRange1,
+        repRange2: repRange2,
+        repRange3: repRange3,
+        type_session: type_session,
+      },
+      { new: true }
+    );
+
+    res.status(202).json(updateExerciseType);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Delete an exercise type
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const deleteExerciseType = await ExerciseType.findOneAndDelete({
+      _id: req.params.id,
+      owner: req.user._id,
+    });
+
+    if (!deleteExerciseType) {
+      return res.status(401).json({
+        message: "Trying to delete Exercise Type - Unauthorized or not found",
+      });
+    }
+    res.status(204).json(deleteExerciseType);
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
