51921c0aa0a917074e9c2707540c8408a9023417
doncarlos5 <pro.julien.thomas@gmail.com>
2024-04-15 17:59:40 +0200

    Refactor exercise-type.model.js to include timestamps

--- models/exercise-type.model.js
+++ models/exercise-type.model.js
@@ -1,39 +1,44 @@
 const { Schema, model } = require("mongoose");
 
-const exerciseType = new Schema({
-  name: {
-    type: String,
-    required: true,
+const exerciseType = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    advice: {
+      type: String,
+    },
+    timer: {
+      type: Number,
+      required: true,
+    },
+    repRange1: {
+      type: String,
+      required: true,
+    },
+    repRange2: {
+      type: String,
+      required: true,
+    },
+    repRange3: {
+      type: String,
+      required: true,
+    },
+    type_session: {
+      type: String,
+      enum: ["Upper A", "Lower", "Upper B"],
+      required: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
   },
-  advice: {
-    type: String,
-  },
-  timer: {
-    type: Number,
-    required: true,
-  },
-  repRange1: {
-    type: String,
-    required: true,
-  },
-  repRange2: {
-    type: String,
-    required: true,
-  },
-  repRange3: {
-    type: String,
-    required: true,
-  },
-  type_session: {
-    type: String,
-    enum: ["Upper A", "Lower", "Upper B"],
-    required: true,
-  },
-  owner: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const ExerciseType = model("ExerciseType", exerciseType);
 
