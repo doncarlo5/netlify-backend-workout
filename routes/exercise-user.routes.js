c7fb44679bf65c8a9630c2a0cbb29d521cd54c2d
doncarlos5 <pro.julien.thomas@gmail.com>
2024-04-16 11:38:21 +0200

    Refactor pagination and sorting logic in exercise-user.routes.js

--- routes/exercise-user.routes.js
+++ routes/exercise-user.routes.js
@@ -43,7 +43,6 @@ router.get("/", async (req, res, next) => {
       .skip(page * limit)
       .limit(limit)
       .sort({ [sortField]: sortOrder });
-
     res.json(exercises);
   } catch (error) {
     next(error);
